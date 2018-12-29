19f03ace3d21291208c880cfac2d58499427e498
mohsen <smohsenmg7@gmail.com>
2018-12-29 16:57:16 +0330

    base of project

---
+++ app/src/main/java/com/example/mohsen/jozveyab/Jozve.java
@@ -0,0 +1,4 @@
+package com.example.mohsen.jozveyab;
+
+public class Jozve {
+}
--- app/src/main/java/com/example/mohsen/jozveyab/MainActivity.java
+++ app/src/main/java/com/example/mohsen/jozveyab/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mohsen.jozveyab;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawer;
@@ -17,17 +20,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
+
+
         drawer = findViewById(R.id.drawer_layout);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navication_drawer_open,
-                R.string.navication_drawer_close);
-
-        toggle.setDrawerIndicatorEnabled(true);
-
+//        باز و بسته شدن منو و آیکون همبرگر
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navication_drawer_open, R.string.navication_drawer_close);
         drawer.addDrawerListener(toggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
         toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_user_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                                , new UserProfileFragment()).commit();
+                    case R.id.nav_category:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                                , new CategoryFragment()).commit();
+                    case R.id.nav_buy:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                                , new BoughtFragment()).commit();
+                    case R.id.nav_download:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                                , new DownloadFragment()).commit();
+                    case R.id.nav_favorite:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                                , new FavoriteFragment()).commit();
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new CategoryFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_category);
+        }
     }
 
     @Override
